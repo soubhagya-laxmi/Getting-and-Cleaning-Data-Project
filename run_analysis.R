d9faf6464a586174e69acd5510fcfc007ec90307
soubhagya Laxmi tripathy <shubbi.tripathy@gmail.com>
2016-10-26 15:10:38 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,13 @@
+# This script will perform the following steps on the UCI HAR Dataset downloaded from 
+# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+# 1. Merge the training and the test sets to create one data set.
+# 2. Extract only the measurements on the mean and standard deviation for each measurement. 
+# 3. Use descriptive activity names to name the activities in the data set
+# 4. Appropriately label the data set with descriptive activity names. 
+# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+
+
+#set working directory to the location where the UCI HAR Dataset was unzipped
 setwd("E:/shubbi study/R program/Corsera/Getting and cleaning data/")
 
 #Reading features.txt
